529e9e43cb43c79d06c0546679898244281c4f3f
wangfan <fan.wang@colorcun.com>
2018-01-17 23:21:57 +0800

    livebc

--- app/Admin/Controllers/LivebcController.php
+++ app/Admin/Controllers/LivebcController.php
@@ -24,10 +24,12 @@ class LivebcController extends Controller
     {
         return Admin::content(function (Content $content) {
 
-            $content->header('header');
-            $content->description('description');
-
-            $content->body($this->grid());
+            $content->header('今日直播');
+            $content->description('');
+            if(Admin::user()->isRole('lecturer')) {
+                $content->body('<div style="max-height: 300px;overflow-y: auto;">'.$this->grid().'</div>');
+                $content->body($this->form());
+            }
         });
     }
 
@@ -41,8 +43,8 @@ class LivebcController extends Controller
     {
         return Admin::content(function (Content $content) use ($id) {
 
-            $content->header('header');
-            $content->description('description');
+            $content->header('今日直播');
+            $content->description('');
 
             $content->body($this->form()->edit($id));
         });
@@ -73,10 +75,28 @@ class LivebcController extends Controller
     {
         return Admin::grid(Livebc::class, function (Grid $grid) {
 
-            $grid->id('ID')->sortable();
+            //disable
+            $grid->disableCreation();
+            $grid->disableExport();
+            $grid->disableRowSelector();
+            $grid->disableActions();
+            $grid->disableFilter();
+            $grid->disablePagination();
+            $grid->model()->where([
+                'expid'=>Admin::user()->id,
+            ]);
+            $grid->model()->orderBy('timestamp','desc');
 
-            $grid->created_at();
-            $grid->updated_at();
+            $grid->column('timestamp','时间')->display(function($timestamp){
+                return substr($timestamp,-8);
+            })->style('width:100px;');
+
+            $grid->tag('类型')->display(function($tag){
+                return Livebc::getTagStr($tag);
+            })->style('width:100px;');
+            $grid->column('content','点评内容')->display(function($html){
+                return $html;
+            });
         });
     }
 
@@ -88,11 +108,17 @@ class LivebcController extends Controller
     protected function form()
     {
         return Admin::form(Livebc::class, function (Form $form) {
-
-            $form->display('id', 'ID');
-
-            $form->display('created_at', 'Created At');
-            $form->display('updated_at', 'Updated At');
+            $form->disableReset();
+            $form->setAction('/admin/livebc');
+            $form->editor('content', '点评内容')->rules('required', [
+                'required' => '必须填写',
+            ]);;
+            $form->time('timestamp', '时间')->default(date('H:i:s',time()));
+            $form->radio('tag','点评类型')->options(Livebc::TAG)->default(0);
+            $form->saving(function(Form $form){
+                $form->timestamp=date('Y-m-d '.$form->timestamp,time());
+                $form->model()->expid=Admin::user()->id;
+            });
         });
     }
 }
--- app/Models/Livebc.php
+++ app/Models/Livebc.php
@@ -6,11 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Livebc extends Model
 {
+    const TAG_PMZS=0;  //盘面综述
+    const TAG_ZPDP=1;  //早盘点评
+    const TAG_WPDP=2;  //午间点评
+    const TAG_SPDP=3;  //收盘点评
+    const TAG_ZDGZ=4;  //重点关注
+    const TAG_RDBK=5;  //热点板块
+    const TAG_TFSJ=6;  //突发事件
+    const TAG_DXTS=7;  //短线提示
+    const TAG=[
+        '盘面综述','早盘点评','午间点评','收盘点评','重点关注','热点板块','突发事件','短线提示'
+    ];
     protected $table = 'livebc';
 
     public $timestamps = false;
     public function expert()
     {
         return $this->hasOne(Expert::class,'expid','expid');
+    }
+    public static function getTagStr($tag=self::TAG_PMZS){
+        if(empty(self::TAG[$tag])){
+            return self::TAG[self::TAG_PMZS];
+        }
+        return self::TAG[$tag];
     }
 }
