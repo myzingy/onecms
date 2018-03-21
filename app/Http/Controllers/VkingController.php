6eb83530bb2fcfd71e4040134d3b75f1873b41ce
wangfan <fan.wang@colorcun.com>
2018-03-21 21:45:58 +0800

    livebc

--- app/Http/Controllers/VkingController.php
+++ app/Http/Controllers/VkingController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use \DB;
 use App\Models\Question;
+use Illuminate\Support\Facades\Input;
 
 class VkingController extends Controller
 {
@@ -16,13 +17,15 @@ class VkingController extends Controller
     }
     public function wang()
     {
-        $res=\DB::table('question')->where('expid',137)->orderByDesc('timestamp')->limit(50)->get();
+        //$input=md5('keyword.'.date("Y-m-d H:i",time()));
+        $input=csrf_token();
         echo <<<END
 <html><head><!--STATUS OK--><meta name="referrer" content="always"><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"/><meta name="format-detection" content="telephone=no"/>
 END;
-
-        foreach ($res as $q){
-            echo <<<END
+        if(Input::get($input)=='778899'){
+            $res=\DB::table('question')->where('expid',137)->orderByDesc('timestamp')->limit(50)->get();
+            foreach ($res as $q){
+                echo <<<END
 <div>
 <p>{$q->timestamp} {$q->asker_name}</p>
 <p>{$q->question}</p>
@@ -31,10 +34,24 @@ END;
 <hr>
 END;
 
+            }
+        }else{
+            echo <<<END
+<form action="" method="post" style="">
+<input type="hidden" name="_token" value="{$input}"/>
+<input name="_method" type="hidden" value="PATCH">
+<input name="{$input}" style="width: 100%; line-height: 50px;font-size: 40px;">
+<br>
+<button type="submit" style="width: 100%; margin: 20px 0; padding: 10px;font-size: 40px;">Test</button>
+</form>
+END;
 
         }
     }
     public function show(){
+        $this->wang();
+    }
+    public function update(){
         $this->wang();
     }
 }
