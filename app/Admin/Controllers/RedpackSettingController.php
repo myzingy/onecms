4de986592f8026a4f9b45b5a6ff5589abad827bc
wangfan <fan.wang@colorcun.com>
2018-04-25 23:51:44 +0800

    livebc

--- app/Admin/Controllers/RedpackSettingController.php
+++ app/Admin/Controllers/RedpackSettingController.php
@@ -3,6 +3,7 @@
 namespace App\Admin\Controllers;
 
 use App\Models\Auth;
+use App\Models\Expert;
 use App\Models\RedpackSetting;
 
 use Encore\Admin\Form;
@@ -11,6 +12,7 @@ use Encore\Admin\Facades\Admin;
 use Encore\Admin\Layout\Content;
 use App\Http\Controllers\Controller;
 use Encore\Admin\Controllers\ModelForm;
+use Illuminate\Support\Facades\Input;
 
 class RedpackSettingController extends Controller
 {
@@ -42,8 +44,8 @@ class RedpackSettingController extends Controller
     {
         return Admin::content(function (Content $content) use ($id) {
 
-            $content->header('header');
-            $content->description('description');
+            $content->header('编辑红包活动');
+            $content->description('');
 
             $content->body($this->form()->edit($id));
         });
@@ -58,8 +60,8 @@ class RedpackSettingController extends Controller
     {
         return Admin::content(function (Content $content) {
 
-            $content->header('header');
-            $content->description('description');
+            $content->header('创建红包活动');
+            $content->description('');
 
             $content->body($this->form());
         });
@@ -77,6 +79,8 @@ class RedpackSettingController extends Controller
             $grid->disableExport();
             $grid->model()->orderBy('actid', 'desc');
             if(Auth::isLecturer()){
+                $grid->disableCreation();
+                $grid->disableActions();
                 $grid->model()->where(['expid'=>Admin::user()->id]);
             }
             $grid->actid('ID')->sortable();
@@ -94,6 +98,18 @@ class RedpackSettingController extends Controller
             $grid->column('countReceiveNum','已领数量')->display(function(){
                 return $this->countReceiveNum();
             });
+
+            $grid->filter(function($filter){
+                // 如果过滤器太多，可以使用弹出模态框来显示过滤器.
+                $filter->useModal();
+                // 禁用id查询框
+                $filter->disableIdFilter();
+                if(!Auth::isLecturer()){
+                    $filter->like('expert.real_name','讲师');
+                }
+                $filter->like('act_name','活动名称');
+
+            });
         });
     }
 
@@ -106,10 +122,40 @@ class RedpackSettingController extends Controller
     {
         return Admin::form(RedpackSetting::class, function (Form $form) {
 
-            $form->display('id', 'ID');
-
-            $form->display('created_at', 'Created At');
-            $form->display('updated_at', 'Updated At');
+            $form->ignore(['expert.real_name','total_feex','start_time','end_time']);
+            $form->hidden('actid');
+            //$form->hidden('expid')->default(Admin::user()->id);
+            $form->text('act_name','红包活动名称')->rules('required|max:45');
+            if(Auth::isLecturer()){
+                $form->hidden('expid')->default(Admin::user()->id);
+            }else{
+                $form->text('expert.real_name', '讲师姓名')->rules('required');
+            }
+            $form->date('start_time','开始日期')->rules('required');
+            $form->date('end_time','结束日期')->rules('required');
+            $form->text('total_feex','总金额')->rules('required|regex:/^[1-9]\d*$/', [
+                'regex' => '必须为数字大于0的整数',
+            ])->default(function() use ($form){
+                return $form->model()->total_fee/100;
+            });
+            $form->text('total_num','总数量')->rules('required|regex:/^[1-9]\d*$/', [
+                'regex' => '必须为数字大于0的整数',
+            ])->default(1);
+            $form->saving(function(Form $form){
+                $form->model()->total_fee=Input::get('total_feex')*100;
+                if($real_name=Input::get('expert.real_name')){
+                    $expert=Expert::where(['real_name'=>$real_name])->first();
+                    if(empty($expert->expid)){
+                        throw new \Exception('讲师真实姓名不存在');
+                    }
+                    $form->model()->expid=$expert->expid;
+                }
+                if(Input::get('start_time')>Input::get('end_time')){
+                    throw new \Exception('开始日期与结束日期设置错误');
+                }
+                $form->model()->start_time=Input::get('start_time')." 12:00:00";
+                $form->model()->end_time=Input::get('end_time')." 23:59:59";
+            });
         });
     }
 }
